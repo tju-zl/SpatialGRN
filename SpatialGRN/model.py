193a7cea650f008ee71bf37c9ada2f950b0195e0
Lei Zhang <tju_zhanglei@outlook.com>
2024-07-25 23:38:25 +0800

    Update model.py

--- SpatialGRN/model.py
+++ SpatialGRN/model.py
@@ -1,5 +1,22 @@
 import torch
+from torch.nn import Module, ModuleList, Transformer, Softmax
 import torch_geometric
 from torch_geometric.nn import MessagePassing
 
+
+class SGRN(Module):
+    def __init__(self, args) -> None:
+        super().__init__()
+        self.args = args
+        
+    def forward(self):
+        pass
+    
+
+class ComputeLoss(Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        
+    def loss(self):
+        pass
 
